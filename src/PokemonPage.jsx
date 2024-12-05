72d31487a4212a5305d96f951bd2a05b5daa9746
Sofia Riekkola <sofia.riekkola@gmail.com>
2024-12-05 15:54:03 +0200

    Delete oopsie log

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -26,8 +26,6 @@ const PokemonPage = ({ previous, next }) => {
   const normalAbility = pokemon.abilities.find((ability) => !ability.is_hidden)
   const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden === true)
 
-  console.log(previous, next)
-
   return (
     <>
       <div className="links">
