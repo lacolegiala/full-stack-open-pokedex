41be6b0c29b4d22fc94526292650d1e8e0941f8b
Sofia Riekkola <sofia.riekkola@gmail.com>
2024-12-23 16:44:07 +0200

    Remove the part that breaks everything

--- app.js
+++ app.js
@@ -14,8 +14,3 @@ app.listen(PORT, () => {
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
-
-app.get('/health', (req, res) => {
-  if (true) throw('error...  ')
-  res.send('ok')
-})
